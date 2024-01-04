92db199cb6b253637fe450d8f05e2b51093759a1
AjayBadrinath <92035508+AjayBadrinath@users.noreply.github.com>
2024-01-05 01:46:36 +0530

    Initial TypeCheck - Yet to add Type safety

--- matrixv1.c
+++ matrixv1.c
@@ -3,47 +3,94 @@
 #include <stdlib.h>
 #include <sys/time.h>
 #include <time.h>
+#include <assert.h>
+
+typedef union  type{
+    mpf_t* f;
+    mpq_t* r;
+    mpz_t* i;
+
+}u_T;
+typedef enum {
+    INT ,
+    RATIONAL,
+    FLOAT
+} e_T;
+
+typedef struct DataType{
+    u_T mpz_type;
+    e_T mpz_types;
+}datatype;
+
 typedef struct mat{
 int row;
 int col;
- mpz_t  *data;
+ datatype  *data;
 }matrix;
-matrix*Create_Matrix(int row,int col,char* type){
+
+matrix*Create_Matrix(int row,int col, datatype* dt){
     if(row>0&&col>0){
         matrix*m=(matrix*)malloc(sizeof(matrix));
         m->row=row;
         m->col=col;
+        m->data=(datatype*)malloc(sizeof(datatype));
+        switch (dt->mpz_types){
+            case INT:
+            printf("%d",18);
+                mpz_t* a=(mpz_t*)malloc(sizeof(mpz_t)*row*col);
+                m->data->mpz_type.i=a;
+                for (int i=0;i<row*col;i++){
+                    mpz_init(m->data->mpz_type.i[i]);
+                    mpz_set_ui(m->data->mpz_type.i[i],0);
+                }
+                break;
+            case RATIONAL:
+                m->data->mpz_type.r=(mpq_t*)malloc(sizeof(mpq_t)*row*col);
+                for (int i=0;i<row*col;i++){
+                    mpq_init(m->data->mpz_type.r[i]);
+                    mpq_set_ui(m->data->mpz_type.r[i],0,0);
+                }
+                break;
 
-        if(type=="int"){
-        
-            m->data=(mpz_t*)malloc(sizeof(mpz_t)*row*col);
-        //printf("%d",m->row);
-            for (int i=0;i<row*col;i++){
-                mpz_init(m->data[i]);
-                mpz_set_ui(m->data[i],0);
-            }  
-            return m;
-        }/*
-        else if(type=="rational"){
-            m->data=(mpq_t*)malloc(sizeof(mpq_t)*row*col);
-        //printf("%d",m->row);
-            for (int i=0;i<row*col;i++){
-                mpq_init(m->data[i]);
-                mpq_set_ui(m->data[i],0,0);
-            }  
-            return m;
+            case FLOAT:
+                m->data->mpz_type.f=(mpf_t*)malloc(sizeof(mpf_t)*row*col);
+                for (int i=0;i<row*col;i++){
+                    mpf_init(m->data->mpz_type.f[i]);
+                    mpf_set_ui(m->data->mpz_type.f[i],0);
+                }
+                break;
+            
         }
-        */
-        else{
-            fprintf(stderr,"Unsupported Type\n");
-            return NULL;
-        }
+        return m;
+
+    
     }else{
         fprintf(stderr,"Matrix rows and colums are indexed form [1....n]\n");
 	    exit(0);
     }
     return NULL;
 }
+void setType(datatype*dt,e_T type,void*val){
+    dt->mpz_types=type;
+    switch(type){
+        case INT:
+            mpz_set(*(dt->mpz_type.i),*(mpz_t*)val);
+    }
+    
+}
+void getType(datatype*dt,e_T type){
+    assert(dt->mpz_types==type);
+    dt->mpz_types=type;
+    switch(type){
+        case INT:
+            return dt->mpz_type.i;
+            break;
+    }
+    return NULL;
+    
+}
+
+
 void insert_element(matrix*mat,int i_row,int j_col,int val){
     mpz_t* m=mat->data;
     mpz_set_ui(*(m+((j_col)+(i_row)*(mat->col))),val);
@@ -53,16 +100,32 @@ int get_element(matrix*mat,int row,int col){
 	    return mpz_get_ui(*(m+((col)+(row)*(mat->col))));
 }
 void print_matrix(matrix*mat){
-    mpz_t * p=mat->data;
-    for(int i=0;i<mat->row*mat->col;i++){
-        if((i%mat->col)==0)printf("\n");
-        printf("%ld\t",mpz_get_ui(*(p++)));
+    switch(mat->data->mpz_types){
+        case INT:
+            printf("\n");
+            mpz_t * p=mat->data->mpz_type.i;
+            for(int i=0;i<mat->row*mat->col;i++){
+                if((i%mat->col)==0)printf("\n");
+                printf("%ld\t",mpz_get_ui(*(p++)));
+            }
+            printf("\n");
+            break;
+        case FLOAT:
+            printf("\n");
+            mpf_t * p_f=mat->data->mpz_type.f;
+            for(int i=0;i<mat->row*mat->col;i++){
+                if((i%mat->col)==0)printf("\n");
+                printf("%ld\t",mpf_get_ui(*(p_f++)));
+            }
+            printf("\n");
+            break;
+
+
     }
-   printf("\n");
 }
 
-matrix* transpose(matrix*mat){
-    matrix*T=Create_Matrix(mat->col,mat->row,"int");
+matrix* transpose(matrix*mat,datatype *dt){
+    matrix*T=Create_Matrix(mat->col,mat->row,dt);
     for(int row=0;row<mat->row;row++){
         for(int col=0;col<mat->col;col++){
             insert_element(T,col,row,get_element(mat,row,col));
@@ -70,22 +133,22 @@ matrix* transpose(matrix*mat){
     }
     return T;
 }
-matrix* Create_Eye_Matrix(int row){
-    matrix* x=Create_Matrix(row,row,"int");
+matrix* Create_Eye_Matrix(int row,datatype *dt){
+    matrix* x=Create_Matrix(row,row,dt);
     for(int i=0;i<=row;i++){
         for(int j=0;j<row;j++){
-            if(i==j)insert_element(x,i,j,1);
+            //if(i==j)insert_element(x,i,j,1);
             
         }
     }
 }
-matrix* cofactor(matrix*x,int a,int b){
-    matrix* tmp=Create_Matrix(x->row-1,x->col-1,"int");
+matrix* cofactor(matrix*x,int a,int b,datatype *dt){
+    matrix* tmp=Create_Matrix(x->row-1,x->col-1,dt);
     int i=1,j=1;
     for(int m=1;m<x->row;m++){
         for(int o=1;o<x->col;o++){
             if(m!=a && o!=b){
-                insert_element(tmp,i,j++,get_element(x,m,o));
+                //insert_element(tmp,i,j++,get_element(x,m,o));
                 if(j==(x->row)){
                     j=1;
                     i++;
@@ -93,7 +156,7 @@ matrix* cofactor(matrix*x,int a,int b){
             }
         }
     }
-    return tmp;
+    //return tmp;
 }
 
 void insert_multival(matrix* x, int *arr){
@@ -107,22 +170,27 @@ void insert_multival(matrix* x, int *arr){
 void main(){
     struct timeval start,end;
     gettimeofday(&start,NULL);
+    datatype * t= malloc(sizeof(datatype));
+    t->mpz_types=FLOAT;
     
-    matrix* k=Create_Matrix(3,5,"int");
+    matrix* k=Create_Matrix(3,5,t);
+    print_matrix(k);
+    /*
     int arr1[]={
         1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
     };
     insert_multival(k,arr1);
-  //  print_matrix(k);
+ 
 
     k=transpose(k);
 
-   // print_matrix(k);
+
     int arr[]={1,2,3,4,5,6,7,8,9};
     matrix*m=Create_Matrix(3,3,"int");
     insert_multival(m,arr);
     matrix*cf=cofactor(m,0,1);
     print_matrix(cf);
+*/
     /*
     for(int i=0;i<3;i++){
         for(int j=0;j<5;j++){
@@ -135,4 +203,5 @@ gettimeofday(&end,NULL);
 long sec=(end.tv_sec-start.tv_sec);
 long us=(((sec*1000000)+end.tv_usec)-(start.tv_usec));
 printf("\nExecutionTime: %lf ms\n",(double)us/1000);
+
 }
